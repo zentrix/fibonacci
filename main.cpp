66bf20ac3cf2f80c964aca1fa957ca0eeed4510c
zentrix.24 <zentrix.24@gmail.com>
2017-09-20 16:04:12 -0400

    creacion de funcion fibs

--- main.cpp
+++ main.cpp
@@ -1,6 +1,25 @@
 #include <iostream>
+#include <cstring>
+#include <cstdlib>
 
-int main() {
-    std::cout << "Hello, World!" << std::endl;
+using namespace std;
+
+int fibs(int position);
+
+int main(int argc, char** argv) {
+  if (argc >= 1){
+    int posicion = atoi(argv[1]);
+    cout << fibs(posicion) << endl;
+  }
     return 0;
+}
+
+int fibs(int position){
+  if(position <= 0){
+    return 0;
+  }
+  else if(position < 2){
+    return 1;
+  }
+  return fibs(position-1) + fibs(position-2);
 }
